0b0d0539225ae43bc9d4c90e4d2679978746dbd5
avaneeshdevkota <avaneeshdevkota86@gmail.com>
2022-10-20 16:48:03 +0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# iris-classification
+# Predicting flower species from labeled dataset
+<p>Dataset downloaded from : <a href = "https://archive.ics.uci.edu/ml/datasets/Iris"> UCI Machine Learning Repository </a></p>
+
+<p>Given 4 numerical attributes, predict what is the type of the flower.</p>
+<p> Uses the K-Nearest Neighbours model with k = 10 to predict the type of flower with roughly 95% accuracy on previously unseen data.</p>
---
+++ main.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import RandomOverSampler
+
+#Column Labels
+cols = ["SLength", "SWidth", "PLength", "PWidth", "Class"]
+df = pd.DataFrame(pd.read_csv("Dataset/iris.data"))
+df.columns = cols
+
+#Separate training, validating and testing data
+#dp.sample shuffles the data and np.split splits 60% into train, 20% into valid and 20% into test
+
+train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
+
+def data_scaler(dataframe, oversample = False):
+
+    # x is our features matrix and y is our target
+    x = dataframe[dataframe.columns[:-1]].values
+    y = dataframe[dataframe.columns[-1]].values
+
+    #Scaler normalizes the data
+    scaler = StandardScaler()
+    x = scaler.fit_transform(x)
+
+    #Equalizes the number of samples of different classes.
+    if (oversample == True):
+
+        ros = RandomOverSampler()
+        x,y = ros.fit_resample(x,y)
+    
+    #Data is the features matrix and target side by side, given by np.hstack
+
+    data = np.hstack((x, np.reshape(y,(-1,1))))
+
+    return data, x, y
+
+#We only wish to check against valid and test so we don't oversample.
+
+train, xtrain, ytrain = data_scaler(train, oversample = True)
+valid, xvalid, yvalid = data_scaler(valid, oversample = False)
+test, xtest, ytest = data_scaler(test, oversample = False)
+
+#K_Nearest_Neighbours
+
+def k_nearest_neighbours(xtrain, ytrain, xtest, ytest):
+
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.metrics import classification_report
+
+    #We compare with the nn closest neighbours.
+    nn = 10
+
+    knn_model = KNeighborsClassifier(n_neighbors= nn)
+    knn_model.fit(xtrain, ytrain)
+
+    ypredictions = knn_model.predict(xtest)
+    report = classification_report(ytest, ypredictions)
+    print(report)
+
+k_nearest_neighbours(xtrain, ytrain, xtest, ytest)
+
